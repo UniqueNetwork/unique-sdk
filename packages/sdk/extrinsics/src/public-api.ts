242c04ad63520d083ccd0efe70b89330d9202074
Arseny Medoev <amedoev@usetech.ru>
2022-05-26 14:49:42 +0300

    lint:fix

--- packages/sdk/extrinsics/src/public-api.ts
+++ packages/sdk/extrinsics/src/public-api.ts
@@ -1,10 +1,10 @@
 import { addFeature } from '@unique-nft/sdk';
 
-export * from './tx';
-export * from './sdk-extrinsics';
-
 import { SdkExtrinsics } from './sdk-extrinsics';
 
 import './augment-sdk';
+
+export * from './tx';
+export * from './sdk-extrinsics';
 
 addFeature('extrinsics', SdkExtrinsics);
