55ac1b1f559896f3bb2cafd784c96d18f936e7e5
AnnaNX1 <achervonyuk@usetech.com>
2022-05-27 18:58:54 +0300

    test create collection

--- packages/web/tests/collection.spec.ts
+++ packages/web/tests/collection.spec.ts
@@ -21,12 +21,59 @@ describe(CollectionController.name, () => {
       expect(ok).toEqual(true);
     });
 
-    it.each(['foo', -1, 1.1])('invalid collectionId', async (collectionId) => {
+    it.each(['foo', -1, 1.1])(
+      'invalid collectionId - %s',
+      async (collectionId) => {
+        const { ok, body } = await request(app.getHttpServer())
+          .get(`/api/collection`)
+          .query({ collectionId });
+        expect(ok).toEqual(false);
+        expect(body.error.code).toEqual(ErrorCodes.Validation);
+      },
+    );
+  });
+
+  describe.skip('POST /api/collection', () => {
+    let generatedCollection;
+    it('generate collection', async () => {
       const { ok, body } = await request(app.getHttpServer())
-        .get(`/api/collection`)
-        .query({ collectionId });
-      expect(ok).toEqual(false);
-      expect(body.error.code).toEqual(ErrorCodes.Validation);
+        .post(`/api/collection`)
+        .send({
+          name: 'Sample collection name',
+          description: 'sample collection description',
+          tokenPrefix: 'TEST',
+          address: 'yGDkQ8CbZSsX6y4PaGC5Q7nVtQxycKunSb3W7dHNAJpNYCzUh',
+        });
+      expect(ok).toEqual(true);
+      generatedCollection = body;
+    });
+
+    let signResponse;
+    it('sign collection', async () => {
+      const { signerPayloadHex } = generatedCollection;
+      const { ok, body } = await request(app.getHttpServer())
+        .post(`/api/extrinsic/sign`)
+        .set({
+          Authorization: 'Seed //Alice',
+        })
+        .send({
+          signerPayloadHex,
+        });
+      expect(ok).toEqual(true);
+      signResponse = body;
+    });
+
+    it('submit collection', async () => {
+      const { signature } = signResponse;
+      const { signerPayloadJSON } = generatedCollection;
+      const { ok, body } = await request(app.getHttpServer())
+        .post(`/api/extrinsic/submit`)
+        .send({
+          signerPayloadJSON,
+          signature,
+          signatureType: 'sr25519',
+        });
+      expect(ok).toEqual(true);
     });
   });
 });
