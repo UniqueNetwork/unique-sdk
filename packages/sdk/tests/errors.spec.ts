34d1a86aa06575a080e38982405343b0e7b46dfa
Arseny Medoev <amedoev@usetech.ru>
2022-06-14 14:42:07 +0300

    remove obsolete describe section

--- packages/sdk/tests/errors.spec.ts
+++ packages/sdk/tests/errors.spec.ts
@@ -5,32 +5,30 @@ import {
 } from '@unique-nft/sdk/errors';
 
 describe('Sdk errors', () => {
-  describe('get from mnemonic ok', () => {
-    const superError = new SdkError(
-      'foo',
-      'bar baz',
-      ErrorCodes.SubmitExtrinsic,
-      SubmitExtrinsicError.name,
-    );
+  const superError = new SdkError(
+    'foo',
+    'bar baz',
+    ErrorCodes.SubmitExtrinsic,
+    SubmitExtrinsicError.name,
+  );
 
-    const wrappedError = SubmitExtrinsicError.wrapError(
-      new Error('foo'),
-      'bar baz',
-    );
+  const wrappedError = SubmitExtrinsicError.wrapError(
+    new Error('foo'),
+    'bar baz',
+  );
 
-    const simpleError = new SubmitExtrinsicError('foo', 'bar baz');
+  const simpleError = new SubmitExtrinsicError('foo', 'bar baz');
 
-    const checkError = (name: string, error: SdkError) => {
-      expect(error.name).toEqual(SubmitExtrinsicError.name);
-      expect(error.code).toEqual(ErrorCodes.SubmitExtrinsic);
-      expect(error.message).toEqual('foo');
-      expect(error.details).toEqual('bar baz');
-    };
+  const checkError = (name: string, error: SdkError) => {
+    expect(error.name).toEqual(SubmitExtrinsicError.name);
+    expect(error.code).toEqual(ErrorCodes.SubmitExtrinsic);
+    expect(error.message).toEqual('foo');
+    expect(error.details).toEqual('bar baz');
+  };
 
-    it.each([
-      ['simpleError', simpleError],
-      ['superError', superError],
-      ['wrappedError', wrappedError],
-    ])('%s', checkError);
-  });
+  it.each([
+    ['simpleError', simpleError],
+    ['superError', superError],
+    ['wrappedError', wrappedError],
+  ])('%s', checkError);
 });
