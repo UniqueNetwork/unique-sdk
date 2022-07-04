2c550f616b55abf003d1d36f652bb626dc1322c1
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2022-07-04 13:14:53 +0300

    rollup base config

--- packages/sdk/rollup.config.js
+++ packages/sdk/rollup.config.js
@@ -1,26 +1,8 @@
-import dts from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-
-import fs from 'fs';
-import path from 'path';
-
-import ts from 'typescript';
-
 const mainPackageJson = require('../../package.json');
 const currentPackageJson = require('./package.json');
+import { createConfig } from '../../rollup.config.base';
 
-const SRC_FOLDER = './packages/sdk';
-const TS_CONFIG = 'tsconfig.lib.json';
-const DIST_FOLDER = './dist/packages/sdk';
-
-const EMBEDDED_DEPS = ['@unique-nft/unique-mainnet-types'];
-
-const checkIsEmbedded = (id) =>
-  EMBEDDED_DEPS.some((embedded) => id.includes(embedded));
-
-const ENTRY_POINTS = [
+const entryPoints = [
   './index.ts',
   './extrinsics/index.ts',
   './balance/index.ts',
@@ -31,154 +13,11 @@ const ENTRY_POINTS = [
   './errors/index.ts',
 ];
 
-let bundlesCount = 0;
-
-const allBundles = [];
-
-const INTERNAL_IDS = ENTRY_POINTS.map((e) => path.join(SRC_FOLDER, e));
-
-const checkIsExternal = (id) => {
-  const isInternal =
-    /^[./]/.test(id) || INTERNAL_IDS.includes(id) || checkIsEmbedded(id);
-
-  return !isInternal;
-};
-
-const parsedTsConfig = ts.readConfigFile(
-  path.join(SRC_FOLDER, TS_CONFIG),
-  (p) => fs.readFileSync(p, 'utf8'),
-).config;
-
-const DTS_CONFIG = {
-  compilerOptions: {
-    paths: parsedTsConfig.compilerOptions.paths,
-    baseUrl: '.',
-  },
-};
-
-const onGenerateBundle = (bundleOptions, bundleData) => {
-  allBundles.push({ options: bundleOptions, bundle: bundleData });
-
-  if (allBundles.length === bundlesCount) {
-    const allImportsSet = new Set();
-    const allExports = {};
-
-    allBundles.forEach(({ options, bundle }) => {
-      const file = `./${options.file}`;
-      const { dir, ext } = path.parse(file);
-
-      Object.values(bundle)[0].imports.forEach((id) => allImportsSet.add(id));
-
-      let pathRequest = path.relative(DIST_FOLDER, dir);
-      pathRequest = pathRequest ? `./${pathRequest}` : '.';
-      const pathResult = `./${path.relative(DIST_FOLDER, file)}`;
-
-      const current = allExports[pathRequest] || {};
-
-      if (ext === '.cjs') {
-        current.require = pathResult;
-      } else if (ext === '.ts') {
-        current.types = pathResult;
-      } else if (ext === '.js') {
-        current.default = pathResult;
-      }
-
-      allExports[pathRequest] = JSON.parse(
-        JSON.stringify(current, ['types', 'require', 'default']),
-      );
-
-      return allExports;
-    });
-
-    const dependencies = Object.entries(mainPackageJson.dependencies).reduce(
-      (acc, [key, value]) =>
-        allImportsSet.has(key) && !checkIsEmbedded(key)
-          ? { ...acc, [key]: value }
-          : acc,
-      {},
-    );
-
-    const rootExports = allExports['.'];
-
-    const packageRoots = {
-      types: rootExports.types.replace('./', ''),
-      main: rootExports.require.replace('./', ''),
-      module: rootExports.default.replace('./', ''),
-    };
-
-    fs.writeFileSync(
-      path.resolve(DIST_FOLDER, 'package.json'),
-      JSON.stringify(
-        {
-          ...mainPackageJson,
-          ...currentPackageJson,
-          dependencies,
-          type: 'module',
-          ...packageRoots,
-          exports: allExports,
-          scripts: undefined,
-          devDependencies: undefined,
-          'lint-staged': undefined,
-          private: undefined,
-        },
-        null,
-        2,
-      ),
-    );
-
-    fs.cpSync(
-      path.resolve(SRC_FOLDER, 'README.md'),
-      path.resolve(DIST_FOLDER, 'README.md'),
-    );
-  }
-};
-
-const afterAll = { generateBundle: onGenerateBundle };
-
-const getEntryConfig = (relativeInput) => {
-  const input = path.join(SRC_FOLDER, relativeInput);
-  const { dir, name } = path.parse(path.join(DIST_FOLDER, relativeInput));
-  const outFile = path.join(dir, name);
-
-  const getBundleConfig = (config) => ({
-    ...config,
-    plugins: [...config.plugins, commonjs(), nodeResolve(), afterAll],
-    input,
-    external: checkIsExternal,
-  });
-
-  return [
-    getBundleConfig({
-      plugins: [esbuild()],
-      output: {
-        file: `${outFile}.cjs`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-    }),
-    getBundleConfig({
-      plugins: [esbuild()],
-      output: {
-        file: `${outFile}.js`,
-        format: 'es',
-        sourcemap: true,
-      },
-    }),
-    getBundleConfig({
-      plugins: [dts(DTS_CONFIG)],
-      output: {
-        file: `${outFile}.d.ts`,
-        format: 'es',
-      },
-    }),
-  ];
-};
-
-const allConfigs = ENTRY_POINTS.reduce(
-  (acc, entry) => [...acc, ...getEntryConfig(entry)],
-  [],
-);
-
-bundlesCount = allConfigs.length;
+const allConfigs = createConfig({
+  packageName: 'sdk',
+  entryPoints,
+  mainPackageJson,
+  currentPackageJson,
+});
 
 export default allConfigs;
