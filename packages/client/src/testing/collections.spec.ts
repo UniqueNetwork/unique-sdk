c3a115babfee709b2c5a37da4dd0e558cc9120f6
AnnaNX1 <achervonyuk@usetech.com>
2022-07-28 17:14:54 +0300

    fix test

--- packages/client/src/testing/collections.spec.ts
+++ packages/client/src/testing/collections.spec.ts
@@ -23,12 +23,10 @@ import { inputDataForCreateCollection } from './values';
 
 const baseUrl = process.env.TEST_WEB_APP_URL || 'http://localhost:3001';
 const TEST_RICH_ACCOUNT = process.env['TEST_RICH_ACCOUNT'] || '//Bob'; // eslint-disable-line
-const TEST_POOR_ACCOUNT = process.env['TEST_POOR_ACCOUNT'] || '//Alice'; // eslint-disable-line
 
 describe('client tests', () => {
   let app: INestApplication;
   let richAccountAddress: string;
-  let poorAccountAddress: string;
   let signer: SdkSigner;
 
   beforeAll(async () => {
@@ -41,8 +39,6 @@ describe('client tests', () => {
     });
     const richAccount = keyringProvider.addSeed(TEST_RICH_ACCOUNT);
     richAccountAddress = richAccount.instance.address;
-    const poorAccount = keyringProvider.addSeed(TEST_POOR_ACCOUNT);
-    poorAccountAddress = poorAccount.instance.address;
     signer = richAccount.getSigner();
   }, 100_000);
 
@@ -51,22 +47,20 @@ describe('client tests', () => {
   });
 
   describe('collections', () => {
-    it('get collection', async () => {
-      const client = new Client({ baseUrl, signer: null });
-      const response: CollectionInfoWithSchemaResponse =
-        await client.collections.collectionByIdFn({
-          collectionId: 10,
-        });
-      expect(response.id).toEqual(expect.any(Number));
-    }, 100_000);
-
-    it('create collection', async () => {
+    it('create and get collection', async () => {
       const client = new Client({ baseUrl, signer });
-      const response: ExtrinsicResultResponse<CreateCollectionParsed> =
+      const createCollectionResponse: ExtrinsicResultResponse<CreateCollectionParsed> =
         await client.collections.createCollectionEx.submitWaitResult(
           inputDataForCreateCollection,
         );
-      expect(response.parsed.collectionId).toEqual(expect.any(Number));
+      expect(createCollectionResponse.parsed.collectionId).toEqual(
+        expect.any(Number),
+      );
+      const getCollectionResponse: CollectionInfoWithSchemaResponse =
+        await client.collections.collectionByIdFn({
+          collectionId: createCollectionResponse.parsed.collectionId,
+        });
+      expect(getCollectionResponse.id).toEqual(expect.any(Number));
     }, 100_000);
   });
 });
