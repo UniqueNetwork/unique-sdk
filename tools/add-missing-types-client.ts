447b8724faed6bb8f7c0b0269106b67774a9bb20
AnnaNX1 <achervonyuk@usetech.com>
2022-07-29 18:24:16 +0300

    fix tools:add-missing-types-client script

--- tools/add-missing-types-client.ts
+++ tools/add-missing-types-client.ts
@@ -3,11 +3,25 @@ import * as fs from 'fs';
 const FIX_FILE = 'packages/client/src/types/api.ts';
 
 function main() {
-  fs.appendFileSync(
-    FIX_FILE,
-    "\nimport { arrayNumberRecordStringAny } from './missingApiTypes';\n",
-    'utf8',
-  );
+  let content = fs.readFileSync(FIX_FILE, 'utf8');
+  content = content
+    .replace(
+      / ExtrinsicResultResponse {/gi,
+      () => ' ExtrinsicResultResponse<T> {',
+    )
+    .replace(
+      /interface ExtrinsicResultResponse[<>\n ;A-Za-z{:]*(parsed\?: object;)/gi,
+      (match) => match.replace('parsed?: object;', 'parsed?: T;'),
+    );
+  if (
+    !content.includes(
+      `import { arrayNumberRecordStringAny } from './missingApiTypes'`,
+    )
+  )
+    content =
+      content +
+      "\nimport { arrayNumberRecordStringAny } from './missingApiTypes';\n";
+  fs.writeFileSync(FIX_FILE, content);
 }
 
 main();
